ee0ba9b71e920c00812cef3e3ca2fcac411739c2
Luan Chrystian Galan <luanchrystian2@gmail.com>
2020-12-03 20:45:46 -0300

    removendo bibliotecas nao utilizadas

--- Pokedex/Controllers/UsuarioController.cs
+++ Pokedex/Controllers/UsuarioController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Pokedex.Dados.EntityFramework.Comum;
 using Pokedex.Models;
 
--- Pokedex/Dados/EntityFramework/Comum/Contexto.cs
+++ Pokedex/Dados/EntityFramework/Comum/Contexto.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Pokedex.Dados.EntityFramework.Configuracao;
 using Pokedex.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Pokedex.Dados.EntityFramework.Comum
 {
